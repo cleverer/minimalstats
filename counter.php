1d92e04453794252428d24ece240dd16a477ddfa
Nicolas Da Mutten <nicolas@damutten.ch>
2017-01-02 17:00:05 +0100

    Adds templates and implements SVG

--- counter.php
+++ counter.php
@@ -11,12 +11,80 @@
 		
 		private function createSVG() {
 			
+			$linePositionValues = array(
+				'left' => array(
+					'line' => array(
+						'x1' => 1,
+						'x2' => 1,
+						'y1' => '0%',
+						'y2' => '100%'
+					),
+					'text' => array(
+						'x' => 6,
+						'y'=> Config::FONT_SIZE+4,
+						'span_x' => Config::WIDTH-4
+					)
+				),
+				'right' => array(
+					'line' => array(
+						'x1' => Config::WIDTH-1,
+						'x2' => Config::WIDTH-1,
+						'y1' => '0%',
+						'y2' => '100%'
+					),
+					'text' => array(
+						'x' => 4,
+						'y'=> Config::FONT_SIZE+4,
+						'span_x' => Config::WIDTH-6
+					)
+				),
+				'top' => array(
+					'line' => array(
+						'x1' => '0%',
+						'x2' => '100%',
+						'y1' => 1,
+						'y2' => 1
+					),
+					'text' => array(
+						'x' => 4,
+						'y'=> Config::FONT_SIZE+4,
+						'span_x' => Config::WIDTH-4
+					)
+				),
+				'bottom' => array(
+					'line' => array(
+						'x1' => '0%',
+						'x2' => '100%',
+						'y1' => Config::HEIGHT-1,
+						'y2' => Config::HEIGHT-1
+					),
+					'text' => array(
+						'x' => 4,
+						'y'=> Config::FONT_SIZE+2,
+						'span_x' => Config::WIDTH-4
+					)
+				)
+
+			);
+			
+			$positions = $linePositionValues[Config::LINE_POSITION];
+			
+			ob_start();
+			include 'templates/counter.php';
+			return ob_get_clean();
 		}
 		
 		private function processRequest() {
 			
 		}
 		
+		function newRequest() {
+			header('Content-Type: image/svg+xml');
+			echo $this->createSVG();
+		}
+		
 	}
 	
-	$counter = new Counter();
+	$counter = new Counter(null, false);
+	
+	$counter->newRequest();
--- minimalstats.php
+++ minimalstats.php
@@ -11,8 +11,11 @@
 	class MS {
 	
 		private $db = null;
+		private $outputPage;
 		
-		function __construct($installer = false) {
+		function __construct($installer = null, $outputPage = true) {
+			
+			$this->outputPage = $outputPage;
 			
 			ob_start();
 			
@@ -24,7 +27,8 @@
 				}
 				$db->set_charset("utf8");
 				$this->db = $db;
-			} else if (!$installer) {
+			} else if ($installer !== true) {
+				$this->outputPage = false;
 				die();
 			}
 		}
@@ -38,38 +42,11 @@
 			$output = ob_get_contents();
 			ob_end_clean();
 			
-			?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta http-equiv="x-ua-compatible" content="ie=edge">
-
-		<title>MinimalStats<?php if(!empty(Config::SITE_NAME)) { echo ": ".Config::SITE_NAME; } if (method_exists($this, 'title')) { echo " – "; $this::title(); }?></title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
-	</head>
-	<body style="padding-top:5rem;">
-		<nav class="navbar navbar-fixed-top navbar-light bg-faded" style="padding-left: 0; padding-right: 0;">
-			<div class="container">
-				<a class="navbar-brand" href="/"><?php if(!empty(Config::SITE_NAME)) { echo Config::SITE_NAME; } else { echo 'MinimalStats'; }?></a>
-				<button class="navbar-toggler hidden-md-up float-xs-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
-				<div class="collapse navbar-toggleable-sm nav navbar-nav" id="navbarResponsive" style="clear: right;">
-					<a class="nav-item nav-link" href="view.php">View</a>
-				</div>
-			</div>
-		</nav>
-		<div class="container">
-			<h1><?php if (method_exists($this, 'title')) { $this::title(); echo ' <small class="text-muted">MinimalStats</small>'; } else { echo 'MinimalStats'; }?></h1>
-			<?php echo $output;?>
-		</div>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
-	</body>
-</html>
-			<?php
-
+			if ($this->outputPage) {
+				include 'templates/basicpage.php';
+			} else {
+				echo $output;
+			}
 		}
 		
 	}
