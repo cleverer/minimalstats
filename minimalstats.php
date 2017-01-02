517d579ce1f2a64f06ad11c0e613357a2f8b1a24
Nicolas Da Mutten <nicolas@damutten.ch>
2017-01-02 13:34:57 +0100

    Refines Titles and layout

--- minimalstats.php
+++ minimalstats.php
@@ -46,13 +46,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 
-		<title>MinimalStats<?php if (method_exists($this, 'title')) echo " – "; $this::title(); ?></title>
+		<title>MinimalStats<?php if(!empty(Config::SITE_NAME)) { echo ": ".Config::SITE_NAME; } if (method_exists($this, 'title')) { echo " – "; $this::title(); }?></title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
 	</head>
 	<body style="padding-top:5rem;">
-		<nav class="navbar navbar-fixed-top navbar-light bg-faded">
+		<nav class="navbar navbar-fixed-top navbar-light bg-faded" style="padding-left: 0; padding-right: 0;">
 			<div class="container">
-				<a class="navbar-brand" href="/"><?php if(!empty(Config::SITE_NAME)) echo Config::SITE_NAME;?></a>
+				<a class="navbar-brand" href="/"><?php if(!empty(Config::SITE_NAME)) { echo Config::SITE_NAME; } else { echo 'MinimalStats'; }?></a>
 				<button class="navbar-toggler hidden-md-up float-xs-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
 				<div class="collapse navbar-toggleable-sm nav navbar-nav" id="navbarResponsive" style="clear: right;">
 					<a class="nav-item nav-link" href="view.php">View</a>
@@ -60,7 +60,7 @@
 			</div>
 		</nav>
 		<div class="container">
-			<h1>MinimalStats<?php if (method_exists($this, 'title')) $this::title(); ?></h1>
+			<h1><?php if (method_exists($this, 'title')) { $this::title(); echo ' <small class="text-muted">MinimalStats</small>'; } else { echo 'MinimalStats'; }?></h1>
 			<?php echo $output;?>
 		</div>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
