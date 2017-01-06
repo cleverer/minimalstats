72183aa03bee89fdfaf7dcab30cf3f143089bf9f
Nicolas Da Mutten <nicolas@damutten.ch>
2017-01-06 20:09:48 +0100

    Updates .gitignore  for config.ini

---
+++ config/config.php
@@ -0,0 +1,43 @@
+<?php
+	// minimalstats v0.1
+	
+	namespace MinimalStats;
+	
+	class Config {
+		
+		//*
+		const DB_HOST = 'localhost';
+		const DB_USER = 'minimalstats';
+		const DB_PASSWORD = 'suMEgo123';
+		const DB_NAME = 'minimalstats';
+		
+		const SITE_NAME = 'nicolas.damutten.ch';
+		
+		const LINE_COLOR = 'forestgreen';
+		const BACKGROUND_COLOR = '#f7f7f9';
+		
+		const LINE_POSITION = 'top';
+		
+		// Probably leave these alone and size the image on your page.
+		const WIDTH = 90;
+		const HEIGHT = 20;
+		const FONT_SIZE = '12px';
+		/*/
+		const DB_HOST = '';
+		const DB_USER = '';
+		const DB_PASSWORD = '';
+		const DB_NAME = '';
+
+		const SITE_NAME = 'Test';
+		
+		const LINE_COLOR = 'green';
+		const BACKGROUND_COLOR = '#f7f7f9';
+		
+		const LINE_POSITION = 'top';
+		
+		// Probably leave these alone and size the image on your page.
+		const WIDTH = 90;
+		const HEIGHT = 20;
+		const FONT_SIZE = '12px';
+		//*/
+	}
